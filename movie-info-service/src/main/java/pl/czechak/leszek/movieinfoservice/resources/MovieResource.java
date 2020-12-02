09da69a54afe000c39a327646a3adbb2b0928fe5
Leszek Czechak <leszekczechak@gmail.com>
2020-12-02 18:34:10 +0100

    configured Tomcat ports and basic controllers

---
+++ movie-info-service/src/main/java/pl/czechak/leszek/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,17 @@
+package pl.czechak.leszek.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.czechak.leszek.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId){
+        return new Movie(movieId, "Test name");
+    }
+}
